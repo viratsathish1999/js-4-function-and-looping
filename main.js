06c4ac29a61c9bbe6b8557a92876eb3901efda3f
viratsathish1999 <sathish.r9688@gmail.com>
2022-12-16 09:49:40 +0530

    add file

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var a,b,c;
+a=prompt("enter text");
+b=parseInt(prompt("HOW MANY TIMES"));
+function swap(a,b){
+  for(i=0;i<b;i++){
+    document.write(a)
+    document.write("<br>")
+  }
+  
+}
+swap(a,b)
